bd6c05e47f030ae89a63edbe7c8554227b9f5804
duyan <duyanhd998@gmail.com>
2025-10-14 08:16:53 +0700

    Vehicle List

---
+++ src/main/java/com/dealermanagementsysstem/project/Model/DAOVehicle.java
@@ -0,0 +1,35 @@
+package com.dealermanagementsysstem.project.Model;
+
+import utils.DBUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOVehicle {
+    public List<DTOVehicle> getVehicles(){
+        List<DTOVehicle> vehicles = new ArrayList<>();
+        String sql = "SELECT VIN, ColorID, ManufactureYear, EngineNumber, CurrentOwner, Status FROM Vehicle";
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery();
+        ) {
+            while (rs.next()) {
+                DTOVehicle v = new DTOVehicle();
+                v.setVIN(rs.getString("VIN"));
+                v.setColorID(rs.getInt("ColorID"));
+                v.setManufactureYear(rs.getInt("ManufactureYear"));
+                v.setEngineNumber(rs.getString("EngineNumber"));
+                v.setCurrentOwner(rs.getString("CurrentOwner"));
+                v.setStatus(rs.getString("Status"));
+                vehicles.add(v);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return vehicles;
+    }
+}
---
+++ src/main/java/com/dealermanagementsysstem/project/controller/VehicleController.java
@@ -0,0 +1,21 @@
+package com.dealermanagementsysstem.project.controller;
+
+import com.dealermanagementsysstem.project.Model.DAOVehicle;
+import com.dealermanagementsysstem.project.Model.DTOVehicle;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class VehicleController {
+    @GetMapping("/vehicleList")
+    public String vehicleList(Model model)
+    {
+        DAOVehicle daoVehicle = new DAOVehicle();
+        List<DTOVehicle> vehicle = daoVehicle.getVehicles();
+        model.addAttribute("vehicleList", vehicle);
+        return "evmPage/vehicleList";
+    }
+}
