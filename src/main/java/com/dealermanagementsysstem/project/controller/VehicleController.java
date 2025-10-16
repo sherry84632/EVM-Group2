16154068f9e660b76cff246e8e4570e47ce426c4
sherry84632 <sherry84632@gmail.com>
2025-10-16 18:37:52 +0700

    Vehicles InformationUI

--- src/main/java/com/dealermanagementsysstem/project/controller/VehicleController.java
+++ src/main/java/com/dealermanagementsysstem/project/controller/VehicleController.java
@@ -18,4 +18,13 @@ public class VehicleController {
         model.addAttribute("vehicleList", vehicle);
         return "evmPage/vehicleList";
     }
+
+    @GetMapping("/getVehicleListToOrder")
+    public String vehicleList2(Model model)
+    {
+        DAOVehicle daoVehicle = new DAOVehicle();
+        List<DTOVehicle> vehicle = daoVehicle.getVehicles();
+        model.addAttribute("vehicleList", vehicle);
+        return "dealerPage/chooseVehicleToOrder";
+    }
 }
